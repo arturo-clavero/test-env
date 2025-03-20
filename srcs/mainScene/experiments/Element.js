7d3fcb7d72ac45e15037ce817141101d3444e719
1Clavero <arturoclavero01@gmail.com>
2025-03-20 19:45:07 +0700

    some extension classes in div

--- srcs/mainScene/experiments/Element.js
+++ srcs/mainScene/experiments/Element.js
@@ -1,102 +1,174 @@
-class Overlay{
+class BaseDivElement{
 	constructor(){
 		this.element = document.createElement('div');
+		this.childElements = [];
+	}
+	add(element){
+		if (! (element instanceof Element))
+		{
+			this.childElements.push(element);
+			element = element.element;
+		}
+		this.element.appendChild(element);
+	}
+	getElementById(id){
+		for (let child of this.childElements) {
+			if (child instanceof BaseDivElement)
+			{
+				let child_found = child.getElementById(id);
+				if (child_found != undefined)
+					return child_found;
+			}
+			if (child.element.id == id)
+				return child;
+		};
+		return undefined;
+	}
+	getElementsWith(type, elements){
+		if (!elements)
+			elements = [];
+		for (let child of this.childElements) {
+			if (child instanceof BaseDivElement)
+				elements = child.getElementsWith(type, elements);
+			else if (child.extensions && child.extensions[type] != undefined)
+				elements.push(child);
+			else
+				console.log("no extensions of ", type, "for: ", child.element);
+		};
+		return elements;
+	}
+}
+
+class Overlay extends BaseDivElement{
+	constructor(children = [], id = "overlay"){
+		super();
+		// this.element = document.createElement('div');
 		this.element.style.backgroundColor = 'transparent';
 		this.element.style.boxSizing = 'border-box';
 		this.element.style.padding = '0';
 		this.element.style.border = 'none';
 		this.element.style.visibility = "hidden";
+		this.element.id = id;
 		document.body.appendChild(this.element);
-	}
-	add(child){
-		if (! (child instanceof Element))
-			child = child.element;
-		this.element.appendChild(child);
+		children.forEach(child => {this.add(child);});
 	}
 }
 
-class FlexBox{
-	constructor({dir, mainAxis = "center", crossAxis = "center", marginTopBottom = 0.5, children, flex = 0, full = false}){
-		this.element = document.createElement('div');
+class FlexBox extends BaseDivElement {
+	constructor({dir, mainAxis = "center", crossAxis = "center", marginTopBottom = 0.5, children = [], flex = 0, full = false, id = "flexbox"}){
+		super();
+		this.element.id = id;
+		// this.element = document.createElement('div');
 		if (full)
 		{
 			this.element.style.height = '100%';
 			this.element.style.width = '100%';
-
 		}
-
 		this.element.style.display = 'flex';
 		this.element.style.flexDirection = dir;
 		this.element.style.justifyContent = mainAxis;
 		this.element.style.alignItems = crossAxis;
 		this.element.style.marginTop = `${marginTopBottom}%`;
 		this.element.style.marginBottom = `${marginTopBottom}%`;
-		if (flex > 0)
-			this.element.style.flex = flex;
-		children.forEach(child => {
-			if (! (child instanceof Element))
-				child = child.element;
-			this.element.appendChild(child);
-		});
+		if (flex > 0) this.element.style.flex = flex;
+		children.forEach(child => {this.add(child);});
 	}
 }
 
-class Text{
-	constructor(content, flex = 0)
+
+
+class BaseNonDivElement {
+	constructor(type, id, flex, fontSize, w, h){
+		this.element =  document.createElement(type);
+		this.element.id = id;
+		if (flex > 0)
+			this.element.style.flex = flex;
+		this.extensions = {
+				text : new TextExtension(this.element, fontSize),
+				size : undefined
+		}
+		if ( w != undefined && h != undefined)
+			this.extensions.size = new SizedExtension(this.element, w, h);
+	}
+
+}
+
+class TextExtension {
+	constructor(element, fontSize){
+		this.element = element;
+		this.fontSizeFactor = fontSize;
+	}
+	updateSize(newFontsize) {
+		console.log("update size for ", this.element);
+		console.log("new : ", newFontsize);
+		console.log("factor: ", this.fontSizeFactor);
+		this.currFontSize = newFontsize * this.fontSizeFactor;
+		console.log("the update is: ", this.currFontSize);
+		this.element.style.fontSize = `${this.currFontSize}px`;
+	}
+	tempChangeSize(factor){
+		this.prevFontSize = this.currFontSize;
+		this.currFontSize *= factor;
+		this.element.style.fontSize = `${this.currFontSize}px`;
+	}
+	revertSize(){
+		this.currFontSize = this.prevFontSize;
+		this.element.style.fontSize = `${this.currFontSize}px`;
+	}
+}
+
+class SizedExtension{
+	constructor(element, width, height){
+		this.element = element;
+		this.widthFactor = width;
+		this.heightFacor = height;
+	}
+	updateSize(newWidth, newHeight){
+		this.element.style.width = `${newWidth * this.widthFactor}px`;
+		this.element.style.height = `${newHeight * this.heightFacor}px`;
+	}
+}
+
+class Text extends BaseNonDivElement{
+	constructor({id = "text", content, flex = 0, fontSize = 1, w, h})
 	{
-		this.element = document.createElement('label');
+		super('label', id, flex, fontSize, w, h);
 		this.element.textContent = content;
-		if (flex > 0)
-			this.element.style.flex = flex;
 	}
 }
 
-class Input{
-	constructor(autofocus = "false", flex = 0)
+class Input extends BaseNonDivElement{
+	constructor({id = "input", autofocus = "false", flex = 0, fontSize = 1, w = 0.9, h = 0.1})
 	{
-		this.element = document.createElement('input');
+		super('input', id, flex, fontSize, w, h);
 		this.element.type = 'text';
 		this.element.autofocus = autofocus;
 		this.element.style.textAlign = 'center';
 		this.element.style.backgroundColor = 'transparent';
 		this.element.style.border = 'none';
 		this.element.style.outline = 'none';
-		if (flex > 0)
-			this.element.style.flex = flex;
 	}
 }
 
-class Button{
-	constructor(content, onClick, flex = 0)
+class Button extends BaseNonDivElement{
+	constructor({id = "button", content, onClick, flex = 0, fontSize = 1, w, h})
 	{
-		this.element = document.createElement('label');
+		super('label', id, flex, fontSize, w, h);
 		// this.element.style.pointerEvents = 'all';
 		this.element.textContent = content;
 		// this.element.style.visibility = "hidden";
-		// this.element.addEventListener('mouseover', ()=>{
-		// 	this.element.className = `${this.className}-hover`;
-		// });
-		// this.element.addEventListener('mouseout', ()=> {
-		// 	this.element.className = this.className;
-		// });
-		console.log("on click ? ", onClick);
-		this.element.addEventListener('click', ()=>{
-			console.log("clicked button");
-			onClick();
-		}); 
-		if (flex > 0)
-			this.element.style.flex = flex;
+		this.element.addEventListener('mouseover', ()=>{ this.extensions.text.tempChangeSize(1.25);});
+		this.element.addEventListener('mouseout', ()=> { this.extensions.text.revertSize(1.25);});
+		this.element.addEventListener('click', ()=>{ onClick();}); 
 	}
 	animate(){
 		const r =  Math.random() * (10);
 		if (r > 9)
 		{
-			if (this.element.style.color === "transparent")
-				this.element.style.color = "black";
-			else
-				this.element.style.color = "transparent";
+			if (this.element.style.color === "transparent") this.element.style.color = "black";
+			else this.element.style.color = "transparent";
 		}
 	}
 }
 
-export {Overlay, FlexBox, Text, Input, Button}
+export {Overlay, FlexBox, Text, Input, Button,}
--- srcs/mainScene/experiments/div1.js
+++ srcs/mainScene/experiments/div1.js
@@ -1,58 +1,48 @@
 
 import { StateManager } from '../state/StateManager';
 import { Overlay, FlexBox, Text, Input, Button} from './Element';
-
-const overlay = new Overlay();
-
-const aliasInput = new Input(true);
-const oponentInput = new Input();
-const enterButton = new Button("ENTER", ()=>{
-	console.log('HELLO!?');
-	const stateManager = new StateManager();
-	stateManager.currentState.changeSubstate();
-});
-
-overlay.add(
+const overlay = new Overlay([
 	new FlexBox({
-		dir: "column", 
-		children: [
-			new FlexBox({
-				dir: "column",
-				mainAxis: "space-evenly",
-				children: [
-					new Text("Your Alias"),
-					aliasInput,
-				],
-				full: true,
-			}),
-			new FlexBox({
-				dir: "column",
-				mainAxis: "space-evenly",
-				children: [
-					new Text("Player 2 Alias"),
-					oponentInput,
-				],
-				full: true,
-			}),
-		],
-		flex: 1,
-		full: true,
-	})
-);
-
-overlay.add(
+			dir: "column", 
+			children: [
+				new FlexBox({
+					dir: "column",
+					mainAxis: "space-evenly",
+					children: [
+							new Text({content: "Your Alias"}),
+							new Input({id: "user-alias", autofocus: true, fontSize: 0.75}),
+					],
+					full: true,
+				}),
+				new FlexBox({
+					dir: "column",
+					mainAxis: "space-evenly",
+					children: [
+							new Text({content: "Player 2 Alias"}),
+							new Input({id: "oponent-alias", fontSize: 0.75}),
+					],
+					full: true,
+				}),
+			],
+			flex: 1,
+			full: true,
+		}),
 	new FlexBox({
-		dir: "row", 
-		mainAxis:"end", 
-		children: [
-			enterButton
-		]
-	})
-);
+			dir: "row", 
+			mainAxis:"end", 
+			children: [
+				new Button({
+					id: "enter-button", 
+					content: "ENTER", 
+					onClick: ()=>{new StateManager().currentState.changeSubstate();}
+				}),
+			]
+		})
+]);
 
-enterButton.element.addEventListener('click', () => {
-	console.log('Button clicked!');
-  });
+const userInput = overlay.getElementById("user-alias");
+const oponentInput = overlay.getElementById("oponent-alias");
+const enterButton = overlay.getElementById("enter-button");
 
 function keyHandler(event){
     if (event.key === 'Enter') {
@@ -66,44 +56,45 @@ function keyHandler(event){
 			return {change : "substate"};
         }
     } else if (event.key === 'ArrowDown') {
-        if (document.activeElement === aliasInput.element)
+        if (document.activeElement === userInput.element)
             oponentInput.element.focus();
     } else if (event.key === 'ArrowUp') {
         if (document.activeElement === oponentInput.element)
-            aliasInput.element.focus();
+            userInput.element.focus();
 			event.preventDefault();
     }
 	else if (document.activeElement != oponentInput.element)
-		aliasInput.element.focus()
+		userInput.element.focus()
 	return undefined;
 }
 
-function resize(){
+function resize()
+{
 	const w = overlay.element.offsetWidth;
 	const h = overlay.element.offsetHeight;
-	aliasInput.element.style.width = `${w * 0.9}px`;
+
+	// overlay.getElementsWith("size").forEach(element => { element.extensions.size.updateSize(w, h);});
+	userInput.element.style.width = `${w * 0.9}px`;
 	oponentInput.element.style.width = `${w * 0.9}px`;
-  	aliasInput.element.style.height = `${h * 0.1}px`;
+  	userInput.element.style.height = `${h * 0.1}px`;
   	oponentInput.element.style.height = `${h * 0.1}px`;
-  	const p = 0.1;
- 	const fontSize = Math.min( w  * p, h * p);
-	overlay.element.style.fontSize = `${fontSize}px`;
-	aliasInput.element.style.fontSize = `${fontSize}px`;
-	oponentInput.element.style.fontSize = `${fontSize}px`;
+	const p = 0.1;
+	const fontSize = Math.min( w  * p, h * p);
+	overlay.getElementsWith("text").forEach(element => { element.extensions.text.updateSize(fontSize);});
 }
 
 function enter(){
-	aliasInput.element.focus();
-	aliasInput.element.value = "";
+	userInput.element.focus();
+	userInput.element.value = "";
 	enterButton.element.style.visibility = "visible";
 }
 
 function exit(){
 	console.log("EXITING!!!");
 	console.log("you: ", aliasInput.element.value, "oponent: ", oponentInput.element.value);
-	aliasInput.element.value = "";
+	userInput.element.value = "";
 	oponentInput.element.value = "";
-	enterButton.style.visibility = "hidden";
+	enterButton.element.style.visibility = "hidden";
 }
 
 function animate(){
