c9486996e844195dfd0e1ccea9b7c1353c3a77d6
1Clavero <arturoclavero01@gmail.com>
2025-04-27 21:19:06 +0700

    single arcade machine

--- src/index.js
+++ src/index.js
@@ -19,8 +19,28 @@ const engine = new MainEngine();
 let isAnimating = false;
 
 console.log("TEST PART");
+const points = [
+    [0, 0],         // [w[0], h[0]]
+    [0, 1],         // [w[0], h[6]]
+    [0.8, 1],       // [w[3], h[7]]
+    [0.8, 0.9],     // [w[3], h[5]]
+    [0.7, 0.85],    // [w[1], h[4]]
+    [0.75, 0.8],    // [w[2], h[3]]
+    [1, 0.6],       // [w[5], h[2]]
+    [1, 0.5],       // [w[5], h[1]]
+    [0.85, 0],      // [w[4], h[1]]
+];
+
+// Function to scale points
+function scale_points(points, wFactor, hFactor) {
+    return points.map(([x, y]) => [
+        x * wFactor,   // Apply width factor
+        y * hFactor    // Apply height factor
+    ]);
+}
+
 const part_test = new Part(
-	[[0, 0], [0, 1], [1, 2], [2, 1], [2, 0]], 
+	scale_points(points, 3, 6), 
 	1,
 	[
 		new THREE.MeshStandardMaterial({ color: 0xff0000, side: THREE.DoubleSide }), // Red
@@ -30,13 +50,13 @@ const part_test = new Part(
 		new THREE.MeshStandardMaterial({ color: 0x0000ff, side: THREE.DoubleSide }), // Blue
 		new THREE.MeshStandardMaterial({ color: 0x800080, side: THREE.DoubleSide }), // Purple
 		new THREE.MeshStandardMaterial({ color: 0xff1493, side: THREE.DoubleSide }), // Pink
-		new THREE.MeshStandardMaterial({ color: 0x000000, side: THREE.DoubleSide }), // Black
+		new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide }), // Black
 	  ]
 	  );
 const test = new Object(part_test);
 test.self.position.x = 0;
 test.self.position.y = 2;
-test.self.position.z = 8;
+test.self.position.z = 4;
 
 //developent:
 document.addEventListener('keydown', (event) => {
@@ -101,7 +121,7 @@ export function uponEnter(){
 }
 
 export function animate() {
-	test.self.rotation.y += 0.001;
+	test.self.rotation.x += 0.01;
 	if (!isAnimating) return ;
 	//console.log("animate");
 	requestAnimationFrame(animate);
